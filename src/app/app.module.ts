e21ee51b2360c77b8fedcc2d76c0f4a088c71315
Danilo Calixto <danilo.calixto@sblogpvh.local>
2022-11-22 13:33:59 -0400

    CursoAngular#45#LocalePipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule , LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
+
+import ptBr from '@angular/common/locales/pt'; // necessário a partir do Angular v5
+import { registerLocaleData } from '@angular/common'; // necessário a partir do Angular v5
+registerLocaleData(ptBr); // necessário a partir do Angular v5
 
 @NgModule({
   declarations: [
@@ -16,7 +21,19 @@ import { CamelCasePipe } from './camel-case.pipe';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    /*{
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+
+    }*/
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService) => settingsService.getLocale()
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
